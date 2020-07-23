8f187c62cee421104f3f63dfa6d159c5445751ed
Himanshu Garg <hgarg701@gmail.com>
2020-07-23 01:34:23 -0700

    Initialized project

---
+++ src/main/java/com/example/redditt/reditt/RedittApplication.java
@@ -0,0 +1,13 @@
+package com.example.redditt.reditt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedittApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/redditt/reditt/RedittApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redditt.reditt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedittApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
