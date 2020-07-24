7c47a2df5719e29e433492ff1b57b0f36d3a7801
Himanshu Garg <hgarg701@gmail.com>
2020-07-24 16:32:20 -0700

    Completed model class Subreddit

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -1,4 +1,36 @@
 package Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long userId;
+
+    @NotBlank(message = "username cannot be blank")
+    private String username;
+
+    @NotBlank(message = "password cannot be blank")
+    private String password;
+
+    @Email
+    @NotEmpty(message = "Email is required")
+    private String email;
+    
+    private boolean enabled;
 }
