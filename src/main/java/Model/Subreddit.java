c36dd535552e7008f1d55bfa1e015da9b19e860f
Himanshu Garg <hgarg701@gmail.com>
2020-07-24 16:25:28 -0700

    Completed model class Subreddit

--- src/main/java/Model/Subreddit.java
+++ src/main/java/Model/Subreddit.java
@@ -1,4 +1,35 @@
 package Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Subreddit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message =  "title cannot be blank")
+    private String title;
+
+    @NotBlank(message = "Description cannot be empty")
+    private String description;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Post> posts;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
