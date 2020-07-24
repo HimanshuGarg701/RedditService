a3cdde1752a28095d30bc22aecd4f58d2ea524d3
Himanshu Garg <hgarg701@gmail.com>
2020-07-24 16:09:19 -0700

    Completed model class Comment

--- src/main/java/Model/Comment.java
+++ src/main/java/Model/Comment.java
@@ -1,4 +1,34 @@
 package Model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String commentText;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
 }
