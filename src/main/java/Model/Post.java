6c8bedcac7c688b716df0444f79d79b3d89251d8
Himanshu Garg <hgarg701@gmail.com>
2020-07-24 15:54:29 -0700

    Completed model class Post

--- src/main/java/Model/Post.java
+++ src/main/java/Model/Post.java
@@ -1,4 +1,43 @@
 package Model;
 
+import com.sun.istack.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @NotBlank(message = "namePost shouldn't be null or empty")
+    private String namePost;
+
+    @Nullable
+    @Lob
+    private String description;
+
+    @Nullable
+    private String url;
+
+    private int numVotes;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
 }
